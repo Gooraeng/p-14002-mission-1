1068642e8384c8dba96fe21397f4bc686c9a2aa5
Gooraeng <birdyoon1998@gmail.com>
2025-08-14 11:17:49 +0900

    Modify Main and HomeController

--- backend/src/main/java/com/back/domain/home/home/controller/HomeController.kt
+++ backend/src/main/java/com/back/domain/home/home/controller/HomeController.kt
@@ -1,46 +1,37 @@
-package com.back.domain.home.home.controller;
+package com.back.domain.home.home.controller
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpSession;
-import lombok.SneakyThrows;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.net.InetAddress;
-import java.util.Collections;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import static java.net.InetAddress.getLocalHost;
-import static org.springframework.http.MediaType.TEXT_HTML_VALUE;
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpSession
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import java.net.InetAddress
 
 @RestController
 @Tag(name = "HomeController", description = "홈 컨트롤러")
-public class HomeController {
-    @SneakyThrows
-    @GetMapping(produces = TEXT_HTML_VALUE)
+class HomeController {
+    @GetMapping(produces = [MediaType.TEXT_HTML_VALUE])
     @Operation(summary = "메인 페이지")
-    public String main() {
-        InetAddress localHost = getLocalHost();
+    fun main(): String {
+        val localHost = InetAddress.getLocalHost()
 
         return """
                 <h1>API 서버</h1>
-                <p>Host Name: %s</p>
-                <p>Host Address: %s</p>
+                <p>Host Name: ${localHost.hostName}</p>
+                <p>Host Address: ${localHost.hostAddress}</p>
                 <div>
                     <a href="/swagger-ui/index.html">API 문서로 이동</a>
                 </div>
-                """.formatted(localHost.getHostName(), localHost.getHostAddress());
+                
+                """.trimIndent()
     }
 
     @GetMapping("/session")
     @Operation(summary = "세션 확인")
-    public Map<String, Object> session(HttpSession session) {
-        return Collections.list(session.getAttributeNames()).stream()
-                .collect(Collectors.toMap(
-                        name -> name,
-                        session::getAttribute
-                ));
+    fun session(session: HttpSession): Map<String, Any> {
+        return session.attributeNames
+            .asSequence()
+            .associateWith {it -> session.getAttribute(it)}
     }
 }
